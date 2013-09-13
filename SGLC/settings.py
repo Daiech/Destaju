921e8772d03b13f96096121d3ff40e4faebd5abf
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-09-13 16:46:12 -0500

    Added allowed hosts

--- SGLC/settings.py
+++ SGLC/settings.py
@@ -3,7 +3,8 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Mauricio Aizaga', 'maizaga@daiech.com'),
+    ('Edwin Mesa', 'emesa@daiech.com'),
 )
 
 URL_BASE = "http://localhost:8000"
@@ -31,7 +32,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["daiech.com", ".daiech.com", ".anunciosuniversitarios.com"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
