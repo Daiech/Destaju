862fb4dd43d300dc1a5375cfcd0f7e739fcadcca
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-09-19 11:48:55 -0500

    Added some models to process_admin

--- apps/process_admin/models.py
+++ apps/process_admin/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfileManager(models.Manager):
+
+    def create_user_profile(self):
+        pass	
+
+
+class UserProfile(models.Model):
+    """Table necessary for create an user account, This serves to validate the email."""
+    dni = models.CharField(max_length=30, verbose_name="dni")
+    phone = models.CharField(max_length=150, verbose_name="phone")
+    cell_phone = models.CharField(max_length=150, verbose_name="cell_phone")
+    city = models.CharField(max_length=150, verbose_name="city")
+    address = models.CharField(max_length=150, verbose_name="address")
+    date_born = models.DateField()
+    is_active_worker = models.BooleanField(default=True)
+
+    id_user = models.OneToOneField(User)
+    id_user_type = models.OneToOneField(UserType)
+    id_employment = models.OneToOneField(Employment)
+    is_active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    objects = ActivationManager()
+
+    def __unicode__(self):
+        return "%s: %s %s" % (self.email, self.activation_key, self.is_expired)
+
+class UserType(models.Model):
+    """Table necessary for create an user account, This serves to validate the email."""
+    name = models.CharField(max_length=150, verbose_name="name")
+    description = models.CharField(max_length=300, verbose_name="description")
+    is_active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    # objects = ActivationManager()
+
+    def __unicode__(self):
+        return "%s: %s %s" % (self.email, self.activation_key, self.is_expired)
+
+class Employment(models.Model):
+    """Table necessary for create an user account, This serves to validate the email."""
+    name = models.CharField(max_length=150, verbose_name="name")
+    description = models.CharField(max_length=300, verbose_name="description")
+    is_active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    # objects = ActivationManager()
+
+    def __unicode__(self):
+        return "%s: %s %s" % (self.email, self.activation_key, self.is_expired)
