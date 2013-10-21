80c95a740b6ff24651606a73effdf93546b56013
edwinfmesa <edwinfmesa@hotmail.com>
2013-10-21 01:07:29 -0500

    ADD module: increases applied

--- apps/payroll/forms.py
+++ apps/payroll/forms.py
@@ -1,20 +1,32 @@
 # encoding:utf-8
 from django import forms
 # from apps.process_admin.models import LegalDiscounts, GeneralDiscounts
-from apps.payroll.models import DiscountsApplied
+from apps.payroll.models import DiscountsApplied, IncreasesApplied
 from django.contrib.auth.models import User
 
 
 class DiscountsAppliedForm(forms.ModelForm):
-    # code = forms.CharField(label="Codigo", widget=forms.TextInput(attrs={'placeholder': 'Codigo', 'autofocus': 'autofocus'}))
-    # name = forms.CharField(label="Nombre", widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
-    # description = forms.CharField(label="Descripcion", widget=forms.Textarea(attrs={'placeholder': 'Descripcion del anuncio'}))
-    # measuring_unit = forms.CharField(label="Unidad de medida", widget=forms.TextInput(attrs={'placeholder': 'Unidad de medida'}))
-    # value = forms.CharField(label="Valor", widget=forms.TextInput(attrs={'placeholder': '','type':"number","step":"any"}))
-    # is_available = forms.BooleanField(label="Disponible", required=False, initial=True)
-    
+
+    def __init__(self, *args, **kwargs):
+        super(DiscountsAppliedForm, self).__init__(*args, **kwargs)
+
+        self.fields['general_discount'].label = "Tipo de descuento"
+        self.fields['value'].label = "Valor"
+
     class Meta:
         model = DiscountsApplied
         fields = ('general_discount', 'value')
 
+
+class IncreasesAppliedForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(IncreasesAppliedForm, self).__init__(*args, **kwargs)
+
+        self.fields['increase'].label = "Tipo de aumento"
+        self.fields['value'].label = "Valor"
+
+    class Meta:
+        model = IncreasesApplied
+        fields = ('increase', 'value')
         
--- apps/payroll/urls.py
+++ apps/payroll/urls.py
@@ -13,4 +13,12 @@ payroll = patterns('apps.payroll.views',
     url(r'^listar-nomina/$', 'list_payroll', name="list_payroll"),
 )
 
-payroll_urls = discounts + payroll
+increases = patterns('apps.payroll.views',
+    url(r'^listar-aumentos-aplicados/$', 'list_increases_applied', name="list_increases_applied"),
+    url(r'^ver-aumentos-aplicados/(?P<id_user>[0-9]+)', 'read_increases_applied', name="read_increases_applied"),
+    url(r'^aplicar-aumento/(?P<id_user>[0-9]+)', 'create_increase_applied', name="create_increase_applied"),
+    url(r'^editar-aumento-aplicado/(?P<id_increase_applied>[0-9]+)', 'update_increase_applied', name="update_increase_applied"),
+    url(r'^eliminar-aumento-aplicado/(?P<id_increase_applied>[0-9]+)', 'delete_increase_applied', name="delete_increase_applied")
+)
+
+payroll_urls = discounts + payroll + increases
