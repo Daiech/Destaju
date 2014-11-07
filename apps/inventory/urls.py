f4c22ffb55e0e6f6260c47023dc59b51dc233360
Edwin Mesa <edwinfmesa@hotmail.com>
2014-11-07 15:26:36 -0500

    add templates

---
+++ apps/inventory/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+
+inventory = patterns('apps.inventory.views',
+    url(r'^listar-inventario/$', 'list_inventory', name="list_inventory"),
+    url(r'^listar-historial-item/$', 'list_item_history', name="list_item_history"),
+    url(r'^listar-ordenes-de-proveedores', 'list_provider_order', name="list_provider_order"),
+    url(r'^listar-ordenes-de-empleados', 'list_employed_order', name="list_employed_order"),
+)
+
+
+inventory_urls = inventory
