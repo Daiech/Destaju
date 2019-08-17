7131dc5fa06b8632c6ce87623a554f9f72b96810
Mauricio Aizaga <mauricioaizaga@gmail.com>
2019-08-16 20:33:47 -0400

    Se permite ejecutar desde un subdominio de mauricioaizaga.com

--- SGLC/settings.py
+++ SGLC/settings.py
@@ -61,7 +61,7 @@ except Exception, e:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [".destaju.com.",".destaju.com.", ".demo.destaju.com.","demo.destaju.com."]
+ALLOWED_HOSTS = ["162.243.207.189", "destaju.mauricioaizaga.com"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
